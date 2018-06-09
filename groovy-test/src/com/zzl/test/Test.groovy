147bd7b9dd41b79f5a3b677f30c12a9063bcd7ad
liang <liang@DESKTOP-OQMP6OB>
2018-06-09 14:48:32 +0800

    groovy

---
+++ groovy-test/src/com/zzl/test/Test.groovy
@@ -0,0 +1,75 @@
+package com.zzl.test
+
+
+/** 
+* 
+* 
+* @author 作者 E-mail: zhiliangmx@163.com
+* @Date 创建时间：2018年6月4日 下午9:44:45 
+*/
+class Test {
+	
+	static main(args) {
+//		println("hello");
+//// 1 可选的类型定义
+//		def version = 1;
+//		
+//// 2 assert 
+//		//assert version == 2;	
+//// 3 括号是可选的
+//		println version
+//		
+//// 4 字符串
+//		def s1 = 'imooc';
+//		def s2 = "gradle  version is ${version}";
+//		def s3 = '''my 
+//name is
+// imooc'''
+//		println(s1)
+//		println(s2)
+//		println(s3)
+//
+		
+		
+//// 5 集合 api
+////list
+//		def buildTools = ['ant','maven'];
+//		buildTools << 'gradle';
+//		assert buildTools.getClass() == ArrayList;
+//		assert buildTools.size() == 3 
+// 
+////map
+//		def buildYears = ['ant':2000,'maven':2004];
+//		buildYears.gradle =- 2009;
+//		println buildYears.ant;
+//		println buildYears['gradle'];
+//		println buildYears.getClass();
+//		
+		
+	}
+//// 闭包
+	//这个要在groovyConsole.bat 里面跑
+	
+	
+//	def c1 = {
+//		v ->
+//			print v;
+//	}
+//	
+//	def c2 = {
+//		print "hello";
+//	}
+//	
+//	def method1(Closure closure) {
+//		closure("param");
+//	}
+//		
+//	def method2(Closure closure) {
+//		closure();
+//	}
+//	method1(c1);
+//	method2(c2);
+	
+	
+}
+ 
---
+++ groovy-test/src/com/zzl/test/projectt.groovy
@@ -0,0 +1,26 @@
+package com.zzl.test 
+/** 
+* 
+* 
+* @author 作者 E-mail: zhiliangmx@163.com
+* @Date 创建时间：2018年6月4日 下午10:28:07 
+*/
+class projectt {
+	
+	static main(args) {
+		
+//		apply plugin:'java';
+//		version = '0.1';
+//		
+//		repositories{
+//			mavenCentral();
+//		}
+//		
+//		dependencies{
+//			compile 'commons-codec:commons-codes:1.6';
+//			
+//		}
+	}
+	
+}
+ 
